bdfd4063b9ecf54a0fba0afefc254ce5b9f92de6
AVISHKA200332 <it23615502@my.sliit.lk>
2026-06-28 11:50:33 +0530

    modify style sheets

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path    = require('path');
+
+const app  = express();
+const PORT = 3000;
+
+// Serve everything in the project root as static files
+app.use(express.static(path.join(__dirname)));
+
+app.listen(PORT, () => {
+  console.log(`EgoTech site running at http://localhost:${PORT}`);
+});
