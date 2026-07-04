320884b7b20f208cb4ca4fc30cd5c695aacfb5db
it23762572 <kaveeshajagoda2023@gmail.com>
2026-07-04 07:25:23 -0700

    credit file update

--- server.js
+++ server.js
@@ -15,6 +15,11 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(ROOT, 'index.html'));
 });
 
+// Support the Credits page under both the correct and older typo-based URLs
+app.get(['/credits.html', '/credits', '/credit.html', '/credit'], (req, res) => {
+  res.sendFile(path.join(ROOT, 'credits.html'));
+});
+
 // Catch-all: serve any .html file by name
 app.get('/:page', (req, res, next) => {
   const file = path.join(ROOT, req.params.page);
